df4458d30727983685beb77532d207c6bf658de1
sherzod-uralov <sherzoduralov432@gmail.com>
2024-09-01 22:48:57 +0500

    any code add

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,189 +1,120 @@
 "use client";
-import React from "react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import Image from "next/image";
-import journal from "@/public/nature-600-min.jpg";
-import { useRegisterUserMutation } from "@/lib/query/register.query";
+import React, { useState, useEffect } from "react";
 import 'react-phone-number-input/style.css';
-import PhoneInput from 'react-phone-number-input';
-import { message } from "antd";
-import Cookies from "js-cookie"; // js-cookie dan import qiling
-import { useRouter } from "next/navigation"; // next/router dan useRouter hookini import qiling
-
-interface IFormInput {
-  phone: string;
-  email: string;
-  password: string;
-  fullName: string;
-  academicDegree: string;
-  workplace: string;
-  profession: string;
-}
+import login from '@/public/log.svg';
+import register from '@/public/register.svg';
+import Image from "next/image";
 
 const Page: React.FC = () => {
-  const { register, handleSubmit, reset, control, formState: { errors }, setValue } = useForm<IFormInput>();
-  const [registerUser, { isLoading, error, isSuccess }] = useRegisterUserMutation();
-  const router = useRouter(); // Router ni oling
+  const [isSignUpMode, setSignUpMode] = useState(false);
 
-  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
-    try {
-      const result = await registerUser({
-        number: data.phone,
-        password: data.password,
-        full_name: data.fullName,
-        science_degree: data.academicDegree,
-        job: data.workplace,
-        place_position: data.profession,
-      }).unwrap();
-
-      console.log("User registered successfully:", result);
-
-      // Tokenni cookie fayliga saqlash
-      if (result?.data?.token) {
-        Cookies.set('access_token', result.token, { expires: 7 }); // Tokenni 7 kunga saqlash
-        message.success("Siz muvaffaqiyatli ro'yxatdan o'tdingiz");
-
-        // "/profile" sahifasiga yo'naltirish
-        router.push('/profile');
-      }
-
-      reset(); // Formani tozalash
-    } catch (err) {
-      console.error("Failed to register user:", err);
-      message.error("Ro'yxatdan o'tishda xatolik yuz berdi");
-    }
+  const handleSignUpClick = () => {
+    setSignUpMode(true);
   };
+
+  const handleSignInClick = () => {
+    setSignUpMode(false);
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-      <div className="container">
-        <div className="flex justify-center items-center mt-10 mb-8">
-          <div className="h-auto mr-10 max-sm:mr-0 shadow-[0.6em_0.6em_1.2em_#d2dce9,-0.5em_-0.5em_1em_#fff] rounded-3xl">
-            <Image
-                src={journal}
-                alt="image"
-                className="w-[500px] h-[700px] rounded-3xl max-md:hidden"
-                width={500}
-                height={500}
-            />
+      <div className={`auth-container ${isSignUpMode ? 'sign-up-mode' : ''}`}>
+        <div className="forms-container">
+          <div className="signin-signup-container">
+            <form action="#" className={`sign-in-form ${isSignUpMode ? 'inactive' : 'active'}`}>
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+            <form action="#" className={`sign-up-form ${isSignUpMode ? 'active' : 'inactive'}`}>
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="btn" value="Sign up" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
           </div>
-          <div>
-            <div className="flex">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col mb-6">
-                  <strong>Telefon Raqamingiz</strong>
-                  <Controller
-                      name="phone"
-                      control={control}
-                      rules={{required: "Iltimos telefon raqamingizni kiriting"}}
-                      render={({field}) => (
-                          <PhoneInput
-                              {...field}
-                              defaultCountry="UZ"
-                              placeholder="Telefon raqamingizni kiriting"
-                              value="+998"
-                              className="border-gray border-[1px] rounded py-2 px-4 pl-2"
-                              onChange={(phone) => setValue("phone", phone || "")}
-                          />
-                      )}
-                  />
-                  {errors.phone && <span>{errors.phone.message}</span>}
-                </div>
+        </div>
 
-                <div className="flex flex-col mb-6">
-                  <strong>Parol</strong>
-                  <input
-                      placeholder="Parolingizni kiriting"
-                      type="password"
-                      {...register("password", {
-                        required: "Iltimos parolingizni kiriting",
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-[100px] pl-2"
-                  />
-                  {errors.password && <span>{errors.password.message}</span>}
-                </div>
-
-                <div className="flex flex-col mb-6">
-                  <strong>To'liq ism, familiya</strong>
-                  <input
-                      placeholder="F.I.SH kiriting"
-                      type="text"
-                      {...register("fullName", {
-                        required: "Iltimos F.I.SH kiriting",
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-[100px] pl-2"
-                  />
-                  {errors.fullName && <span>{errors.fullName.message}</span>}
-                </div>
-
-                <div className="flex flex-col mb-6">
-                  <strong>Ilmiy daraja va unvoningiz</strong>
-                  <input
-                      placeholder="Ilmiy daraja va unvoningizni kiriting"
-                      type="text"
-                      {...register("academicDegree", {
-                        required: "Iltimos ilmiy daraja va unvoningizni kiriting",
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-[100px] pl-2"
-                  />
-                  {errors.academicDegree && <span>{errors.academicDegree.message}</span>}
-                </div>
-
-                <div className="flex flex-col mb-6">
-                  <strong>Ish joyi</strong>
-                  <input
-                      placeholder="Ish joyini kiriting"
-                      type="text"
-                      {...register("workplace", {
-                        required: "Iltimos ish joyini kiriting",
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-[100px] pl-2"
-                  />
-                  {errors.workplace && <span>{errors.workplace.message}</span>}
-                </div>
-
-                <div className="flex flex-col mb-6">
-                  <strong>Kasb</strong>
-                  <input
-                      placeholder="Kasbingizni kiriting"
-                      type="text"
-                      {...register("profession", {
-                        required: "Iltimos kasbingizni kiriting",
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-[100px] pl-2"
-                  />
-                  {errors.profession && <span>{errors.profession.message}</span>}
-                </div>
-
-                <div className="flex flex-col mb-8">
-                  <strong>Elektron Pochta</strong>
-                  <input
-                      placeholder="Elektron pochtangizni kiriting"
-                      type="email"
-                      {...register("email", {
-                        required: "Iltimos elektron pochtangizni kiriting",
-                        pattern: {
-                          value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                          message: "Elektron pochта notogri formatda kirgizilgan",
-                        },
-                      })}
-                      className="border-gray border-[1px] rounded py-2 px-4 pl-2"
-                  />
-                  {errors.email && <span>{errors.email.message}</span>}
-                </div>
-
-                <button
-                    type="submit"
-                    className="bg-blue-600 text-white rounded px-[120px] mt-6 py-2"
-                    disabled={isLoading} // Блокируем кнопку, пока идет загрузка
-                >
-                  <strong>
-                    {isLoading ? "Loading..." : "Ro'yxatdan O'tish"}
-                  </strong>
-                </button>
-                {/* Отображаем ошибку, если она есть */}
-                {isSuccess && <p className="text-green-500">User registered
-                  successfully!</p>} {/* Уведомление об успешной регистрации */}
-              </form>
+        <div className="panels-container">
+          <div className="auth-panel auth-left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                ex ratione. Aliquid!
+              </p>
+              <button className="btn transparent" id="sign-up-btn" onClick={handleSignUpClick}>
+                Sign up
+              </button>
             </div>
+            <Image src={login} className="image" alt="" />
+          </div>
+          <div className="auth-panel auth-right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button className="btn transparent" id="sign-in-btn" onClick={handleSignInClick}>
+                Sign in
+              </button>
+            </div>
+            <Image src={register} className="image" alt="" />
           </div>
         </div>
       </div>
