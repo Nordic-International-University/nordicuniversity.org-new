0e905b094d58136edcf21edf55399ad1f5d109f6
Abdulaziz2003333 <lboy21044@gmail.com>
2024-09-01 21:20:40 +0500

    to Vercel

--- app/publications/volume/[volumeId]/page.tsx
+++ app/publications/volume/[volumeId]/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import BigArticlesCard from "@/app/components/Cards/BigArticlesCard";
+import { redirect } from "next/navigation";
 
 export async function generateStaticParams() {
   const res = await fetch("https://journal2.nordicun.uz/category");
@@ -18,12 +19,12 @@ export async function generateStaticParams() {
 }
 
 const PublicationPage = async ({
-  params,
-}: {
+                                 params,
+                               }: {
   params: { volumeId: string };
 }) => {
   const res = await fetch(
-    `https://journal2.nordicun.uz/article/user/volume/${params.volumeId}`,
+      `https://journal2.nordicun.uz/article/user/volume/${params.volumeId}`,
   );
 
   if (!res.ok) {
@@ -32,23 +33,29 @@ const PublicationPage = async ({
 
   const categoryData = await res.json();
 
+  // Check if there is no data returned from the API
+  if (!categoryData || categoryData.length === 0) {
+    redirect("/publications/empty");
+    return null; // To prevent further rendering
+  }
+
   return (
-    <div className="container">
-      <div className="grid grid-cols-4 gap-4 max-sm:grid-cols-1 max-lg:grid-cols-2">
-        {categoryData.map((article: any, index: number) => (
-          <BigArticlesCard
-            key={index}
-            title={article.title}
-            date={article.createdAt}
-            slug={article.slug}
-            category={article.category.name}
-            description={article.description}
-            author={article.author.full_name}
-            imageUrl={article.image.file_path}
-          />
-        ))}
+      <div className="container">
+        <div className="grid grid-cols-4 gap-4 max-sm:grid-cols-1 max-lg:grid-cols-2">
+          {categoryData.map((article: any, index: number) => (
+              <BigArticlesCard
+                  key={index}
+                  title={article.title}
+                  date={article.createdAt}
+                  slug={article.slug}
+                  category={article.category.name}
+                  description={article.description}
+                  author={article.author.full_name}
+                  imageUrl={article.image.file_path}
+              />
+          ))}
+        </div>
       </div>
-    </div>
   );
 };
 
