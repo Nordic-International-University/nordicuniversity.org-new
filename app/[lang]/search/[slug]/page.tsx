c464a450733c8dfa53fe6119ef58b5b18a4be357
Nordic-International-University <info@nordicuniversity.org>
2024-11-25 11:11:41 +0500

    new push1

--- app/[lang]/search/[slug]/page.tsx
+++ app/[lang]/search/[slug]/page.tsx
@@ -21,7 +21,7 @@ interface RouteData {
 }
 const SearchResults = async (slug: string, lang: string) => {
   const result = await fetch(
-    `http://localhost:3000/api/search?query=${slug}&lang=${lang}`,
+    `http://localhost:3778/api/search?query=${slug}&lang=${lang}`,
     { cache: "no-store" },
   );
   return await result.json();
