a15aeeb689d6023bd9a9d6c20b5b8df178b088b9
Nordic-International-University <info@nordicuniversity.org>
2024-12-02 18:07:19 +0500

    final

--- app/api/generate-news/route.ts
+++ app/api/generate-news/route.ts
@@ -1,27 +1,27 @@
-// // app/api/sitemap-news/route.ts
-// import { NextResponse } from "next/server";
-// import fetch from "node-fetch";
-// import { generateSitemap } from "@/app/helpers/generate-sitemap"; // API so'rovini yuborish uchun
-//
-// // API GET request handler
-// export async function GET() {
-//   try {
-//     // API'dan yangiliklarni olish
-//     const response = await fetch(
-//       "https://yourdomain.com/api/press/news?language=uz&page=1&limit=100",
-//     );
-//     const newsData = await response.json();
-//
-//     if (newsData && Array.isArray(newsData)) {
-//       await generateSitemap(newsData); // generateSitemap funksiyasini chaqirish
-//     }
-//
-//     return NextResponse.json({ message: "Sitemap generated successfully" });
-//   } catch (error) {
-//     console.error("Failed to fetch news or generate sitemap:", error);
-//     return NextResponse.json(
-//       { error: "Failed to generate sitemap" },
-//       { status: 500 },
-//     );
-//   }
-// }
+// app/api/sitemap-news/route.ts
+import { NextResponse } from "next/server";
+import fetch from "node-fetch";
+import { generateSitemap } from "@/app/helpers/generate-sitemap"; // API so'rovini yuborish uchun
+
+// API GET request handler
+export async function GET() {
+  try {
+    // API'dan yangiliklarni olish
+    const response = await fetch(
+      "https://yourdomain.com/api/press/news?language=uz&page=1&limit=100",
+    );
+    const newsData = await response.json();
+
+    if (newsData && Array.isArray(newsData)) {
+      await generateSitemap(newsData); // generateSitemap funksiyasini chaqirish
+    }
+
+    return NextResponse.json({ message: "Sitemap generated successfully" });
+  } catch (error) {
+    console.error("Failed to fetch news or generate sitemap:", error);
+    return NextResponse.json(
+      { error: "Failed to generate sitemap" },
+      { status: 500 },
+    );
+  }
+}
