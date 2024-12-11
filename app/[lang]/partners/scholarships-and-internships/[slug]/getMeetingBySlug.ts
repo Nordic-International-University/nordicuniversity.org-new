1ec920baf382d775c8b6ffd25e15c6a18b513ed3
Nordic-International-University <info@nordicuniversity.org>
2024-12-11 16:32:02 +0500

    change port

--- app/[lang]/partners/scholarships-and-internships/[slug]/getMeetingBySlug.ts
+++ app/[lang]/partners/scholarships-and-internships/[slug]/getMeetingBySlug.ts
@@ -1,8 +1,9 @@
-const getMeetingBySlug = async (slug: string, lang: string) => {
+const getMeetingBySlug = async (slug: string, lang: string, headers = {}) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_URL_BACKEND}/api/cooperation/meeting/${slug}?language=${lang}`,
     {
       cache: "no-cache",
+      headers,
     },
   );
   return await response.json();
