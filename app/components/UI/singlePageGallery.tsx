df700c77d63efbdef16977ab47b274995f0a06d7
Nordic-International-University <info@nordicuniversity.org>
2025-01-28 10:59:20 +0300

    add partners add snow effect

--- app/components/UI/singlePageGallery.tsx
+++ app/components/UI/singlePageGallery.tsx
@@ -5,20 +5,22 @@ import { ImageData } from "@/types/templates/single.image.galleery.types";
 
 const ImageGrid = ({ images }: any) => {
   return (
-    <div className="grid grid-cols-1 mt-5 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {images.map((img: ImageData, index: number) => (
-        <div className="w-full" key={img.image_id || index}>
-          <Image
-            className="w-full h-[300px] object-cover"
-            src={process.env.NEXT_PUBLIC_URL_BACKEND + img.image.file_path}
-            alt={img.image.file_name}
-            preview={{
-              src: process.env.NEXT_PUBLIC_URL_BACKEND + img.image.file_path,
-            }}
-          />
-        </div>
-      ))}
-    </div>
+    <Image.PreviewGroup>
+      <div className="grid grid-cols-1 mt-5 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {images.map((img: ImageData, index: number) => (
+          <div className="w-full" key={img.image_id || index}>
+            <Image
+              className="w-full h-[300px] object-cover"
+              src={process.env.NEXT_PUBLIC_URL_BACKEND + img.image.file_path}
+              alt={img.image.file_name}
+              preview={{
+                src: process.env.NEXT_PUBLIC_URL_BACKEND + img.image.file_path,
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </Image.PreviewGroup>
   );
 };
 
