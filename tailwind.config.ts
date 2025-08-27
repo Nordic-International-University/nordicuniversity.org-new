a410c48ceff1b103fd823e0914037de1b855f0ce
sherzod uralov <sherzoduralov432@gmail.com>
2025-08-27 16:03:55 +0500

    add url and color problem solve

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,6 +47,7 @@ const config: Config = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        //test
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
